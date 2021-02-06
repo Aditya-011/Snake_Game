61bb447f99632e75c27e9b964038bdabdc8ac684
Aditya-011 <adihgr18@gmail.com>
2021-02-06 23:29:16 +0530

    deployed to server

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# Snake-JavaScript
+# src
 
-The Snake game, created using JavaScript, and The HTML5 canvas.
+This project was built with Turbo 360. To learn more, click here: https://www.turbo360.co
 
-Download the starter template, and follow the tutorial on youtube step by step.
+## Instructions
+After cloning into repo, cd to project root directory and install dependencies:
 
-Tutorial link : https://youtu.be/9TcU2C1AACw
+```
+$ npm install
+```
+
+To run dev server, install Turbo CLI globally:
+
+```
+$ sudo npm install turbo-cli -g
+```
+
+Then run devserver from project root directory:
+
+```
+$ turbo devserver
+```
+
+To build for production, run build:
+
+```
+$ npm run build
+```
--- public/js/snake.js
+++ public/js/snake.js
@@ -12,10 +12,10 @@ const box = 32;
 // load images
 
 const ground = new Image();
-ground.src = "img/ground.png";
+ground.src = "images/ground.png";
 
 const foodImg = new Image();
-foodImg.src = "img/food.png";
+foodImg.src = "images/food.png";
 
 // load audio files
 
@@ -38,16 +38,16 @@ down.src = "audio/down.mp3";
 let snake = [];
 
 snake[0] = {
-    x : 9 * box,
-    y : 10 * box
+    x: 9 * box,
+    y: 10 * box,
 };
 
 // create the food
 
 let food = {
-    x : Math.floor(Math.random()*17+1) * box,
-    y : Math.floor(Math.random()*15+3) * box
-}
+    x: Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 3) * box,
+};
 
 // create the score var
 
@@ -57,20 +57,20 @@ let score = 0;
 
 let d;
 
-document.addEventListener("keydown",direction);
+document.addEventListener("keydown", direction);
 
-function direction(event){
+function direction(event) {
     let key = event.keyCode;
-    if( key == 37 && d != "RIGHT"){
+    if (key == 37 && d != "RIGHT") {
         left.play();
         d = "LEFT";
-    }else if(key == 38 && d != "DOWN"){
+    } else if (key == 38 && d != "DOWN") {
         d = "UP";
         up.play();
-    }else if(key == 39 && d != "LEFT"){
+    } else if (key == 39 && d != "LEFT") {
         d = "RIGHT";
         right.play();
-    }else if(key == 40 && d != "UP"){
+    } else if (key == 40 && d != "UP") {
         d = "DOWN";
         down.play();
     }
@@ -78,118 +78,103 @@ function direction(event){
 }
 
 // cheack collision function
-function collision(head,array){
-    for(let i = 0; i < array.length; i++){
-        if(head.x == array[i].x && head.y == array[i].y){
+function collision(head, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (head.x == array[i].x && head.y == array[i].y) {
             return true;
         }
     }
-   
+
     return false;
-    
 }
 
 // draw everything to the canvas
 
-function draw(){
-    
-    ctx.drawImage(ground,0,0);
-    
-    for( let i = 0; i < snake.length ; i++){
-        ctx.fillStyle = ( i == 0 )? "green" : "white";
-        ctx.fillRect(snake[i].x,snake[i].y,box,box);
-        
+function draw() {
+    ctx.drawImage(ground, 0, 0);
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = i == 0 ? "green" : "white";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+
         ctx.strokeStyle = "red";
-        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
     }
-    
+
     ctx.drawImage(foodImg, food.x, food.y);
-    
+
     // old head position
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
-    
+
     // which direction
-    if( d == "LEFT") snakeX -= box;
-    if( d == "UP") snakeY -= box;
-    if( d == "RIGHT") snakeX += box;
-    if( d == "DOWN") snakeY += box;
-    
+    if (d == "LEFT") snakeX -= box;
+    if (d == "UP") snakeY -= box;
+    if (d == "RIGHT") snakeX += box;
+    if (d == "DOWN") snakeY += box;
+
     // if the snake eats the food
-    if(snakeX == food.x && snakeY == food.y){
+    if (snakeX == food.x && snakeY == food.y) {
         score++;
         eat.play();
         food = {
-            x : Math.floor(Math.random()*17+1) * box,
-            y : Math.floor(Math.random()*15+3) * box
-        }
+            x: Math.floor(Math.random() * 17 + 1) * box,
+            y: Math.floor(Math.random() * 15 + 3) * box,
+        };
         // we don't remove the tail
-    }else{
+    } else {
         // remove the tail
         snake.pop();
     }
-    
+
     // add new Head
-    
+
     let newHead = {
-        x : snakeX,
-        y : snakeY
-    }
-    
+        x: snakeX,
+        y: snakeY,
+    };
+
     // game over
-    
-    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
+
+    if (
+        snakeX < box ||
+        snakeX > 17 * box ||
+        snakeY < 3 * box ||
+        snakeY > 17 * box ||
+        collision(newHead, snake)
+    ) {
         clearInterval(game);
         dead.play();
         enableScroll();
     }
-    
+
     snake.unshift(newHead);
-    
+
     ctx.fillStyle = "white";
     ctx.font = "45px Changa one";
-    ctx.fillText(score,2*box,1.6*box);
+    ctx.fillText(score, 2 * box, 1.6 * box);
 }
 
 // call draw function every 100 ms
 
-let game = setInterval(draw,100);
+let game = setInterval(draw, 100);
 
-const restart = () =>
-{
+const restart = () => {
     location.reload();
+};
+
+document.addEventListener("keypress", (event) => {
+    if (event.keyCode == 13 || event.keyCode == 32) {
+        restart();
+    }
+});
+
+function disableScroll() {
+    document.body.classList.add("stop-scrolling");
 }
 
-document.addEventListener('keypress',(event)=>
-{
-        if(event.keyCode == 13 || event.keyCode == 32)
-        {
-            restart();
-        }
-})
-function disableScroll() { 
-    document.body.classList.add("stop-scrolling");
-} 
-
-function enableScroll() { 
-    document.body.classList.remove("stop-scrolling"); 
+function enableScroll() {
+    document.body.classList.remove("stop-scrolling");
     document.body.classList.add("start-scrolling");
-    console.log('hello')
-} 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log("hello");
+}
